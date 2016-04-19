68cbe51b4e803c74c473fa884272ddae5132cddc
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2016-04-19 01:40:56 -0400

    Bug 394984: Fix write permission check in OSX updater. r=me

--- toolkit/xre/updaterfileutils_osx.mm
+++ toolkit/xre/updaterfileutils_osx.mm
@@ -6,7 +6,6 @@
 #include "updaterfileutils_osx.h"
 
 #include <Cocoa/Cocoa.h>
-#include <pwd.h>
 
 bool IsRecursivelyWritable(const char* aPath)
 {
@@ -30,27 +29,7 @@ bool IsRecursivelyWritable(const char* aPath)
 
   for (NSString* currPath in paths) {
     NSString* child = [rootPath stringByAppendingPathComponent:currPath];
-    NSDictionary* attributes =
-      [fileManager attributesOfItemAtPath:child
-                                    error:&error];
-    if (error) {
-      [pool drain];
-      return false;
-    }
-    NSNumber* accountID =
-      (NSNumber*)[attributes valueForKey:NSFileOwnerAccountID];
-    NSNumber* groupID =
-      (NSNumber*)[attributes valueForKey:NSFileGroupOwnerAccountID];
-    NSNumber* permissions =
-      (NSNumber*)[attributes valueForKey:NSFilePosixPermissions];
-    int perms = [permissions shortValue];
-    // Check if the user has write access to the file. This is the case when
-    // the user is the owner of the file and the owner has write access, or if
-    // the user belongs to a group that has write access to the file (typically
-    // "admin").
-    if (!((unsigned int)[accountID shortValue] == getuid() && (perms & 0x80)) &&
-        !((unsigned int)[groupID shortValue] == (getpwuid(getuid()))->pw_gid &&
-            (perms & 0x10))) {
+    if ([fileManager isWritableFileAtPath:child] == NO) {
       [pool drain];
       return false;
     }
