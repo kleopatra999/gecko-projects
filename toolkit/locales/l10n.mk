8a9ffb79c1f6acd87b94ba8f1ea981333d969f2d
Justin Wood <Callek@gmail.com>
2016-06-22 08:59:07 -0400

    Bug 1276110 - Workaround a wget bug by not performing internationalisation. r=ted
    Taskclusters cloud-mirror sends a url-encoded response as a redirect, which allows it to point at the absolute url to the file in another AWS region and then it fetches and caches it while returning said file to our client (wget in this case)
    Wget however is performing ASCII-->UTF8 conversion on the url on windows, while at the same time also performing a urldecodeon the redirected url.
    This urldecode is breaking Taskclusters cloud-mirror, so we work around that by telling wget not to perform the conversion at all (we don't need charset conversion here)
    
    Alternative option was to set --local-encoding=ASCII, however that option doesn't work on our current linux machines (and likely OSX builders, but untested there) while --no-iri works on all 3 platforms, so we are going with that.
    
    MozReview-Commit-ID: EXmiDpozFNr
    
    --HG--
    extra : source : 37edfd2c53edf8829992fb418b0f27b676f5b830

--- toolkit/locales/l10n.mk
+++ toolkit/locales/l10n.mk
@@ -189,14 +189,14 @@ wget-en-US:
 ifndef WGET
 	$(error Wget not installed)
 endif
-	$(NSINSTALL) -D $(_ABS_DIST)/$(PKG_PATH)
-	(cd $(_ABS_DIST)/$(PKG_PATH) && $(WGET) --no-cache -nv -N  '$(EN_US_BINARY_URL)/$(PACKAGE)')
-	@echo 'Downloaded $(EN_US_BINARY_URL)/$(PACKAGE) to $(_ABS_DIST)/$(PKG_PATH)/$(PACKAGE)'
+	$(NSINSTALL) -D $(ABS_DIST)/$(PKG_PATH)
+	(cd $(ABS_DIST)/$(PKG_PATH) && $(WGET) --no-cache -nv --no-iri -N  '$(EN_US_BINARY_URL)/$(PACKAGE)')
+	@echo 'Downloaded $(EN_US_BINARY_URL)/$(PACKAGE) to $(ABS_DIST)/$(PKG_PATH)/$(PACKAGE)'
 ifdef RETRIEVE_WINDOWS_INSTALLER
 ifeq ($(OS_ARCH), WINNT)
-	$(NSINSTALL) -D $(_ABS_DIST)/$(PKG_INST_PATH)
-	(cd $(_ABS_DIST)/$(PKG_INST_PATH) && $(WGET) --no-cache -nv -N '$(EN_US_BINARY_URL)/$(PKG_PATH)$(PKG_INST_BASENAME).exe')
-	@echo 'Downloaded $(EN_US_BINARY_URL)/$(PKG_PATH)$(PKG_INST_BASENAME).exe to $(_ABS_DIST)/$(PKG_INST_PATH)$(PKG_INST_BASENAME).exe'
+	$(NSINSTALL) -D $(ABS_DIST)/$(PKG_INST_PATH)
+	(cd $(ABS_DIST)/$(PKG_INST_PATH) && $(WGET) --no-cache -nv --no-iri -N '$(EN_US_BINARY_URL)/$(PKG_PATH)$(PKG_INST_BASENAME).exe')
+	@echo 'Downloaded $(EN_US_BINARY_URL)/$(PKG_PATH)$(PKG_INST_BASENAME).exe to $(ABS_DIST)/$(PKG_INST_PATH)$(PKG_INST_BASENAME).exe'
 endif
 endif
 
